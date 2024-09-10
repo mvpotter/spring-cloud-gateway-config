4d5650709d26867e5a8a0a3c74b13c79de8b8f96
mpotter <mikhail.potter@diabolocom.com>
2024-09-10 22:06:22 +0200

    Add simple routes

---
+++ src/main/java/com/mvpotter/api_gateway_demo/ApiGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvpotter.api_gateway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvpotter/api_gateway_demo/ApiGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvpotter.api_gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
